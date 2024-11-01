967a681ec6f0e921ddc061aae7d088c2660d88b1
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-11-01 23:22:29 +0300

    I added empty constructor

--- Result.cs
+++ Result.cs
@@ -15,13 +15,15 @@ public sealed class Result<T>
     [JsonIgnore]
     public int StatusCode { get; set; } = (int)HttpStatusCode.OK;
 
-    [JsonConstructor]
+    public Result()
+    {
+    }
+
     public Result(T data)
     {
         Data = data;
     }
 
-    [JsonConstructor]
     public Result(int statusCode, List<string> errorMessages)
     {
         IsSuccessful = false;
@@ -29,7 +31,6 @@ public sealed class Result<T>
         ErrorMessages = errorMessages;
     }
 
-    [JsonConstructor]
     public Result(int statusCode, string errorMessage)
     {
         IsSuccessful = false;
